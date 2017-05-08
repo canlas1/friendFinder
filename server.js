c92677ddd4eefffd880e6f4dcd4693eca90c9df7
canlas1 <dc@uscr.com>
2017-05-07 21:04:59 -0400

    updated html and server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
+
 var express = require('express');
-var favicon = require('express-favicon');
+// var favicon = require('express-favicon');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express(); 
@@ -9,23 +10,18 @@ app.use(express.static(__dirname + '/public'));
 var PORT = process.env.PORT || 8080;
 
 // BodyParser makes it easy for our server to interpret data sent to it.
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// app.use(favicon(__dirname + '/app/public/favicon.png'));
-// app.use(express.static(__dirname + '/app/public'));
 
 
 // ROUTER
-
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 // LISTENER
-
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
